9138b6b9b64b8f1515b16641e40b7b4ef96d9dae
etsai <etsai2@gmail.com>
2013-04-17 23:49:08 -0700

    Setup connection properties and blocking if max connection is exceeded

--- src/com/github/etsai/utils/sql/ConnectionPool.java
+++ src/com/github/etsai/utils/sql/ConnectionPool.java
@@ -5,11 +5,17 @@
 package com.github.etsai.utils.sql;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.PriorityQueue;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -26,25 +32,60 @@ public class ConnectionPool {
         }
     }
     
-    private ConnectionBuilder builder;
+    private int maxConnections;
+    private String url;
+    private Properties dbProps;
     private PriorityQueue<ConnectionInfo> availableConnections;
     private Map<Connection, ConnectionInfo> usedConnections;
     
-    public ConnectionPool(ConnectionBuilder builder) {
-        this.builder= builder;
+    public ConnectionPool(int maxConnections) {
+        this.maxConnections= maxConnections;
+        dbProps= new Properties();
     }
-    public void release(Connection conn) {
+    public ConnectionPool() {
+        this(5);
+    }
+    public void setJdbcUrl(String url) {
+        this.url= url;
+    }
+    public void setDbUser(String user) {
+        dbProps.setProperty("user", user);
+    }
+    public void setDbPassword(String password) {
+        dbProps.setProperty("password", password);
+    }
+    public void setMaxConnections(int maxConnections) {
+        this.maxConnections= maxConnections;
+    }
+    public synchronized void release(Connection conn) {
         availableConnections.add(usedConnections.get(conn));
         usedConnections.remove(conn);
+        notifyAll();
     }
-    public Connection getConnection() {
+    public synchronized Connection getConnection() throws SQLException {
         if (availableConnections.isEmpty()) {
-            ConnectionInfo connInfo= new ConnectionInfo();
-            connInfo.lastUsed= Calendar.getInstance().getTime();
-            connInfo.connection= builder.create();
-            availableConnections.add(connInfo);
+            if (usedConnections.keySet().size() >= maxConnections) {
+                try {
+                    wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(ConnectionPool.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                ConnectionInfo connInfo= new ConnectionInfo();
+                connInfo.lastUsed= Calendar.getInstance().getTime();
+                connInfo.connection= DriverManager.getConnection(url, dbProps);
+                availableConnections.add(connInfo);
+            }
         }
         usedConnections.put(availableConnections.peek().connection, availableConnections.peek());
         return availableConnections.poll().connection;
+    }
+    public void close() throws SQLException {
+        while(!availableConnections.isEmpty()) {
+            availableConnections.poll().connection.close();
+        }
+        for(Entry<Connection, ConnectionInfo> entry: usedConnections.entrySet()) {
+            entry.getKey().close();
+        }
     }
 }
